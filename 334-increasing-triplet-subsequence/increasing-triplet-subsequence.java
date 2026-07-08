3ec22bb7f3c29e4100f0fdfff8945ee4067b997e
Vaibhav Gusain <vaibhav3152.beai25@chitkara.edu.in>
2026-07-08 15:27:45 +0530

    Time: 2 ms (99.51%) | Memory: 122.7 MB (22.90%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -2,11 +2,7 @@ class Solution {
     public boolean increasingTriplet(int[] nums) {
         int i=Integer.MAX_VALUE;
         int j=Integer.MAX_VALUE;        
-         System.gc();
-        if(nums.length < 3){
-            return false;
-        }
-        else{
+        
         for(int a =0;a<nums.length;a++){
         int ele = nums[a];
         if(i>=ele){
@@ -20,6 +16,5 @@ class Solution {
         }
         }
     return false;
-    }
     }
 }
