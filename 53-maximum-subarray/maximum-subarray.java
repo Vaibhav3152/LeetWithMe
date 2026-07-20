891787a1d5470ebf218e8c3edfd8ab444bee5aa6
Vaibhav Gusain <vaibhav3152.beai25@chitkara.edu.in>
2026-07-20 16:45:58 +0530

    Time: 1 ms (99.95%) | Memory: 77.5 MB (14.17%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("Array cannot be null or empty.");
+        }
+        int currSum = nums[0];
+        int max = nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(currSum+nums[i]>nums[i]){
+                currSum+=nums[i];
+            }
+            else{
+                currSum = nums[i];
+            }
+            max = Math.max(max,currSum);
+        }
+        return max;
+    }
+}
